e85e38e7de7b78999233ed53e866f98027975030
h3xxit <43811028+h3xxit@users.noreply.github.com>
2020-04-06 12:16:29 +0300

    Const

---
+++ Desktop/Aplicatie Inventariere/Aplicatie Inventariere/Const.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicatie_Inventariere
+{
+    public static class Const
+    {
+
+    }
+}
