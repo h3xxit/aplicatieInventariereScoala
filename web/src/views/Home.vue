a47fd1c38a5da7ebfde64dad51e31caba94ee6fe
Andrei <andreibv96@yahoo.com>
2020-04-06 14:16:58 +0300

    Start

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  
+  <div>
+    <MainPage />
+  </div>
 </template>
 
 <script>
+import MainPage from '@/components/MainPage.vue'
+
 export default {
-    name: Home
+  name: 'Home',
+  components: {
+    MainPage
+  }
 }
 </script>
-
-<style>
-
-</style>
