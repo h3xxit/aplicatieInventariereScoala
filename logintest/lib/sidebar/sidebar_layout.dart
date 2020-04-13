dc0281afde734ec5a47630c497e026e3d025189d
CristinaCri <cri.m.comaniciu@gmail.com>
2020-04-13 13:42:01 +0300

    masterryy

--- logintest/lib/sidebar/sidebar_layout.dart
+++ logintest/lib/sidebar/sidebar_layout.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sidebar_animation/pages/homepage.dart';
-import 'package:sidebar_animation/sidebar/sidebar.dart';
+import 'package:logintest/pages/homepage.dart';
+import 'package:logintest/sidebar/sidebar.dart';
 
 import 'sidebar.dart';
 
